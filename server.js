28998dc0d7a6ad6e9fe9d0f7c0fcd6176bf9ddb8
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-06 13:23:47 -0500

    body & query

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+/* const router = express.Router(); */ // currently is not necesary
 
 var app = express();
 
-app.use('/', function (req, res) {
+/* app.use(router); */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/message', (req, res) => {
+  console.log(req.body)
+  console.log(req.query)
+  res.send('Lista de mensajes ' + req.body.text);
+})
+app.post('/message', (req, res) => {
+  res.send('Mensaje añadido con exito');
+})
+
+/* app.use('/', function (req, res) {
   res.send('Hola <3');
-});
+}); */
 
 app.listen(3000);
 
