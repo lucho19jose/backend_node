b47cb4aa7edebe5440a52b1968effc2b2e644a6d
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-08 09:28:46 -0500

    connect db

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,23 @@
-const list = [];
+require('dotenv').config()
+const db = require('mongoose');
+const Model = require('./model');
+db.Promise = global.Promise;
+
+db.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tcwke.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+}).then(() => { console.log('[db] success connected') })
+  .catch((e) => { console.log(e) })
+
+
 
 function addMessage(message) {
-  list.push(message);
+  /* list.push(message); */
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  return await Model.find();
 }
 
 module.exports = {
